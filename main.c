5b15030493811bfdae714bd5aebe710ceac5ac70
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-06-16 00:58:54 +0800

    some changes

--- main.c
+++ main.c
@@ -3,137 +3,257 @@
 
 #include <stdio.h>
 
+#define WIDTH 5
+#define HEIGHT 7
+
+#define CLEAR "\e[1;1H\e[2J"
+
 typedef int bool;
 
-void NextPlayerMove (bool ok, bool aTurn, int prev, int next, char P[][7])
+void NextPlayerMove(bool *ok, bool *aTurn, int prev, int next, char P[HEIGHT][WIDTH], int *numA, int *numB)
 {
+	
 	int a, b, c, d;
 	
-	a = prev / 10;
-	b = prev % 10;
-	c = next / 10;
-	d = next  % 10;
+	a = prev % 10;
+	b = prev / 10;
+	c = next % 10;
+	d = next / 10;
 
-	if  (aTurn && P[a - 1][b - 1] == 'A' && a == c + 1 && ( d == b || d == b + 1 || b == d + 1))
-		ok = !ok;
-	else if (!aTurn && P[a - 1][b - 1] == 'B' && c== a + 1 && ( d == b || d == b + 1 || b == d + 1))
-		ok = !ok;
-	if (ok && aTurn && P[c - 1][d - 1] == 'F')
+	if  ((*aTurn) && P[a - 1][b - 1] == 'A' && a == c + 1 && ( d == b || d == b + 1 || b == d + 1))
+		(*ok) = !(*ok);
+	else if (!(*aTurn) && P[a - 1][b - 1] == 'B' && c== a + 1 && ( d == b || d == b + 1 || b == d + 1))
+		(*ok) = !(*ok);
+	
+	if ((*ok) && (*aTurn) && P[c - 1][d - 1] == 'F')
 	{
 		P[a - 1][b - 1] = 'F';
 		P[c - 1][d - 1] = 'A';
-		aTurn = !aTurn;
-		ok = !ok;
+		(*aTurn) = !(*aTurn);
+		(*ok) = !(*ok);
 	}
-	else if (ok && !aTurn && P[c - 1][d - 1] == 'F')
+	else if ((*ok) && !(*aTurn) && P[c - 1][d - 1] == 'F')
 	{
 		P[a - 1][b - 1] = 'F';
 		P[c - 1][d - 1] = 'B';
-		aTurn = !aTurn;
-		ok = !ok;
-	}
-	if (ok && aTurn && P[c - 1][d - 1] == 'B' && !(c % 2 == d % 2))
-		ok = !ok;
-	else if (ok && aTurn && P[c - 1][d - 1] == 'B' && (c % 2 == d % 2))
-	{
-		P[c - 1][d - 1] = 'F';
-		P[a - 1][b - 1] = 'F';
-		P[c - 1][d - 1] = 'A';
-		aTurn = !aTurn;
-		ok = !ok;
-	}
-	if (ok && !aTurn && P[c - 1][d - 1] == 'A' && !(c % 2 == d % 2))
-		ok = !ok;
-	else if (ok && aTurn && P[c - 1][d - 1] == 'B' && (c % 2 == d % 2))
-	{
-		P[c - 1][d - 1] = 'F';
-		P[a - 1][b - 1] = 'F';
-		P[c - 1][d - 1] = 'B';
-		aTurn = !aTurn;
-		ok = !ok;
-	}
-}
-
-bool GameOver (bool over, char P[][7])
-{
-	int i, j;
-	bool zero = false;
-
-	// |Beta| = 0
-	for (i = 0; i < 5; i++)
-		for (j = 0; j < 7 && !zero; j++)
-			if (P[i][j] == 'B')
-				zero = true;
-	if (zero)
-	{
-		printf("Alpha Wins");
-		return true;
-	}
-
-	// |Alpha| = 0
-	zero = false;
-	for (i = 0; i < 5; i++)
-		for (j = 0; j < 7 && !zero; j++)
-			if (P[i][j] == 'A')
-				zero = true;
-	if (zero)
-	{
-		printf("Beta Wins");
-		return true;
+		(*aTurn) = !(*aTurn);
+		(*ok) = !(*ok);
 	}
 	
-	// Alpha   |Alpha- Y| = 0
-	zero = true;
-for (i = 0; i < 5; i++)
-		for (j = 0; j < 7 && zero; j++)
-			if (P[i][j] == 'A' && !((i % 2 == j % 2) && i >= 2))
-			zero = false;
-	if (zero)
+	if ((*ok) && (*aTurn) && P[c - 1][d - 1] == 'B' && !(c % 2 == d % 2))
+		(*ok) = !(*ok);
+	else if ((*ok) && (*aTurn) && P[c - 1][d - 1] == 'B' && (c % 2 == d % 2))
 	{
-		printf("Alpha Wins");
-		return true;
+		P[c - 1][d - 1] = 'F';
+		P[a - 1][b - 1] = 'F';
+		P[c - 1][d - 1] = 'A';
+		(*aTurn) = !(*aTurn);
+		(*ok) = !(*ok);
+
+		(*numB)--;
 	}
 
-	// Beta   |Beta- E| = 0
-	zero = true;
-	for (i = 0; i < 5; i++)
-		for (j = 0; j < 7 && zero; j++)
-			if (P[i][j] == 'A' && !((i % 2 == j % 2) && i <= 6))
-			zero = false;
-	if (zero)
+	if ((*ok) && !(*aTurn) && P[c - 1][d - 1] == 'A' && !(c % 2 == d % 2))
+		(*ok) = !(*ok);
+	else if ((*ok) && !(*aTurn) && P[c - 1][d - 1] == 'A' && (c % 2 == d % 2))
 	{
-		printf("Beta Wins");
-		return true;
-	}
+		P[c - 1][d - 1] = 'F';
+		P[a - 1][b - 1] = 'F';
+		P[c - 1][d - 1] = 'B';
+		(*aTurn) = !(*aTurn);
+		(*ok) = !(*ok);
 
-	return false;
+		(*numA)--;
+	}
 }
 
-void Display (char P[7][5])
+// bool GameOver (char P[HEIGHT][WIDTH], int numA, int numB)
+// {
+// 	int i, j;
+// 	bool zero = false;
+
+// 	// |Beta| = 0
+// 	for (i = 0; i < 5; i++)
+// 		for (j = 0; j < 7 && !zero; j++)
+// 			if (P[i][j] == 'B')
+// 				zero = true;
+// 	if (zero)
+// 	{
+// 		printf("Alpha Wins");
+// 		return true;
+// 	}
+
+// 	// |Alpha| = 0
+// 	zero = false;
+// 	for (i = 0; i < 5; i++)
+// 		for (j = 0; j < 7 && !zero; j++)
+// 			if (P[i][j] == 'A')
+// 				zero = true;
+// 	if (zero)
+// 	{
+// 		printf("Beta Wins");
+// 		return true;
+// 	}
+	
+// 	// Alpha   |Alpha- Y| = 0
+// 	zero = true;
+// 	for (i = 0; i < 5; i++)
+// 		for (j = 0; j < 7 && zero; j++)
+// 			if (P[i][j] == 'A' && !((i % 2 == j % 2) && i >= 2))
+// 			zero = false;
+// 	if (zero)
+// 	{
+// 		printf("Alpha Wins");
+// 		return true;
+// 	}
+
+// 	// Beta   |Beta- E| = 0
+// 	zero = true;
+// 	for (i = 0; i < 5; i++)
+// 		for (j = 0; j < 7 && zero; j++)
+// 			if (P[i][j] == 'A' && !((i % 2 == j % 2) && i <= 6))
+// 			zero = false;
+// 	if (zero)
+// 	{
+// 		printf("Beta Wins");
+// 		return true;
+// 	}
+
+// 	return false;
+// }
+
+bool GameOver(char P[HEIGHT][WIDTH], int numA, int numB)
 {
-	int i;
-	int line = 196;
-	for(i = 0; i < 20; i ++)
-		printf("%c", line);
-	printf("\n");
-	printf("|  %c  |  %c  |  %c  |  %c  |  %c  |\n", P[0][0], P[1][0], P[2][0], P[3][0], P[4][0]);
-	printf("|  %c  |  %c  |  %c  |  %c  |  %c  |\n", P[0][1], P[1][1], P[2][1], P[3][1], P[4][1]);
-	printf("|  %c  |  %c  |  %c  |  %c  |  %c  |\n", P[0][2], P[1][2], P[2][2], P[3][2], P[4][2]);
-	printf("|  %c  |  %c  |  %c  |  %c  |  %c  |\n", P[0][3], P[1][3], P[2][3], P[3][3], P[4][3]);
-	printf("|  %c  |  %c  |  %c  |  %c  |  %c  |\n", P[0][4], P[1][4], P[2][4], P[3][4], P[4][4]);
-	printf("|  %c  |  %c  |  %c  |  %c  |  %c  |\n", P[0][5], P[1][5], P[2][5], P[3][5], P[4][5]);
-	printf("|  %c  |  %c  |  %c  |  %c  |  %c  |\n", P[0][6], P[1][6], P[2][6], P[3][6], P[4][6]);
-	for(i = 0; i < 20; i ++)
-		printf("%c", line);
-	printf("\n");
+	int nCurrHeight;
+	int nCurrWidth;
+
+	bool winA = false;
+	bool winB = false;
+
+	if (numA == 0)
+	{
+		printf(CLEAR);
+		printf("Beta Wins!");
+		return true;
+	}
+	else if (numB == 0)
+	{
+		printf(CLEAR);
+		printf("Alpha wins!");
+		return true;
+	}
+	else
+	{
+		for (nCurrHeight = 0; nCurrHeight < 2; nCurrHeight++)
+		{
+			for (nCurrWidth = nCurrHeight % 2; nCurrWidth < WIDTH; nCurrWidth += 2)
+			{
+				if (P[nCurrHeight][nCurrWidth] == 'A')
+					numA--;
+			}
+		}
+
+		if (numA > 0)
+		{
+			for (nCurrHeight = 5; nCurrHeight < 7; nCurrHeight++)
+			{
+				for (nCurrWidth = nCurrHeight % 2; nCurrWidth < WIDTH; nCurrWidth += 2)
+				{
+					if (P[nCurrHeight][nCurrWidth] == 'B')
+						numB--;
+				}
+			}
+			if (numB <= 0)
+			{
+				printf(CLEAR);
+				printf("Beta wins!");
+				winB = true;
+			}
+		}
+		else
+		{
+			printf(CLEAR);
+			printf("Alpha wins!");
+			winA = true;
+		}
+	}
+
+	return (winA || winB);
 }
+
+void Display (char P[HEIGHT][WIDTH])
+{
+	int nCurrWidth;
+	int nCurrHeight;
+
+	// numbers at top
+	for (nCurrWidth = 0 ; nCurrWidth < (WIDTH * 2) + 1; nCurrWidth++)
+	{
+		if (nCurrWidth % 2 != 0)
+		{
+			printf("%d", ((nCurrWidth - 1) / 2) + 1);
+		}
+		else
+			printf(" ");
+	}
+
+	printf("\n");
+
+	for (nCurrHeight = 0 ; nCurrHeight < (HEIGHT * 2) + 1; nCurrHeight++)
+	{
+
+		for (nCurrWidth = 0; nCurrWidth < (WIDTH * 2) + 1; nCurrWidth++)
+		{
+			if (nCurrWidth % 2 == 0)
+			{
+				if (nCurrHeight % 2 == 0)
+				{
+					printf(" ");
+				}
+				else
+				{
+					printf("|");
+
+					if (nCurrWidth == (WIDTH * 2))
+						printf(" %d", ((nCurrHeight - 1) / 2) + 1);
+				}
+			}
+
+			else
+			{
+				if (nCurrHeight % 2 == 0)
+				{
+					printf("%c", 196);  
+				}
+				else
+				{
+					if (P[(nCurrHeight - 1) / 2][(nCurrWidth - 1) / 2] == 'F')
+						printf(" ");
+
+					else
+					{
+						printf("%c", P[(nCurrHeight - 1) / 2][(nCurrWidth - 1) / 2]);
+					}
+				}
+			}
+		}
+		
+		printf("\n");
+	}
+	
+}
+
 int main () 
 {
 	bool over = false;
-	bool ok = false;
 	bool aTurn = true;
+	bool ok = false;
 	
-	char P[7][5] = 
+	int numA = 5;
+	int numB = 5;
+
+
+	char P[HEIGHT][WIDTH] = 
 	{
 		{'B', 'F', 'B', 'F', 'B'},
 		{'F', 'B', 'F', 'B', 'F'},
@@ -146,12 +266,38 @@ int main ()
 	int prev;
 	int next;
 	
-	Display(P);
+	while (!over)
+	{
+		printf(CLEAR);
 
-	printf("Please enter prev.");
-	scanf(" %i", &prev);
-	printf("Please enter next.");
-	scanf(" %i", &next);
+		Display(P);
 
+		printf("\n");
 
+		if (aTurn)
+			printf("Alpha's Turn\n");
+		else
+			printf ("Beta's Turn\n");
+
+		printf("\n\n");
+	
+		printf("Please enter prev: ");
+		scanf(" %i", &prev);
+		printf("Please enter next: ");
+		scanf(" %i", &next);
+		
+		NextPlayerMove(&ok, &aTurn, prev, next, P, &numA, &numB);
+
+		over = GameOver(P, numA, numB);
+
+		if (over)
+		{
+			printf("\n\n");
+			Display(P);
+		}
+	}
+	
+	
+
+	return 0;
 }
